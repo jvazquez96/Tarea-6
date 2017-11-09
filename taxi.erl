c3b0d2a5da560f5945e7199b60e020895f8fb967
miguelbanda <miguelangelbandardz@gmail.com>
2017-11-09 17:01:46 -0600

    Taxi client

--- taxi.erl
+++ taxi.erl
@@ -1,1 +1,22 @@
 -module(taxi).
+-export(central/0, reportar/).
+
+% nombre largo del servidor (nombre@máquina)
+central() -> 'central@MiguelBanda'.
+
+% funciones de interfase
+reportar(Quien, Placas, Modelo) ->
+	llama_central({Quien, Placas, Modelo}).
+
+% cliente taxi
+llama_central(Registro) ->
+	Central = central(),
+	monitor_node(Central, true),
+	{servidor_central, Central} ! {self(), Registro},
+	receive
+	{servidor_central, Respuesta} ->
+		monitor_node(Central, false),
+		Respuesta;
+	{nodedown, Central} ->
+	no
+	end.
